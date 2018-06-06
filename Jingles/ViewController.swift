5e64d63244c9cb9e729735cda7f5c338907e85e0
Mini31 <mini31@copperadmins-mac-mini.local>
2018-06-06 12:15:56 +0530

    Initial Commit

---
+++ Jingles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jingles
+//
+//  Created by Mini31 on 06/06/18.
+//  Copyright © 2018 Copper Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
